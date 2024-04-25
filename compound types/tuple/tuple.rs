820ac326f0a00968a912724edcd5b61e25ad5035
Kaan <kaan.evcimen@sky.uk>
2024-04-25 12:00:34 +0100

    Tuple

---
+++ compound types/tuple/tuple.rs
@@ -0,0 +1,11 @@
+// elements in a tuple can have diff types
+
+fn main() {
+    let _t0: (u8, i16) = (0, -1);
+    let _t1: (u8, (i16, u32)) = (0, (-1, 1));
+
+    let t: (u8, u16, i64, &str, String) = (1u8, 2u16, 3i64, "hello", String::from(", world"));
+
+    println!("Success!");
+
+}
---
+++ compound types/tuple/tuple2.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let t = ("i", "am", "sunface");
+    assert_eq!(t.2, "sunface"); // extracting 3rd element of t
+
+    println!("Success!");
+}
