25fe9650c46aa2ecda509a2c31f24e9a880e69d6
Kaan <kaan.evcimen@sky.uk>
2024-04-25 11:33:29 +0100

    Array

---
+++ compound types/array/array.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let arr: [i32; 5] = [1, 2, 3, 4, 5]; // initializing array of 5 x i32 values
+
+    assert!(arr.len() == 5);
+
+    println!("Success!");
+}
---
+++ compound types/array/array4.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let arr = ['a', 'b', 'c'];
+    
+    let ele = arr[0]; // same as in Python
+
+    assert!(ele == 'a');
+
+    println!("Success!");
+}
